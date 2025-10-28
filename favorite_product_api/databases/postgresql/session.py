7ece786d038a38cf2ebdfa213e5f106d590435f3
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-28 17:41:10 -0300

    feat: adiciona arquivo session.py para criar sessões assíncronas com o banco de dados

---
+++ favorite_product_api/databases/postgresql/session.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from favorite_product_api.settings import settings
+
+engine = create_async_engine(
+    url=settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+)
+
+Session = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Fornece uma sessão assíncrona do banco de dados
+
+    Yields:
+        AsyncSession: Sessão assíncrona do banco de dados.
+    """
+    async with Session() as db_session:
+        try:
+            yield db_session
+            await db_session.commit()
+        except Exception:
+            await db_session.rollback()
+            raise
