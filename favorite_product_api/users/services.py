16247d12fed3f43e0cc40231cf56ffc79a1e0c5b
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-29 20:55:58 -0300

    refactor: não retorna nenhum objeto ao deletar um user

--- favorite_product_api/users/services.py
+++ favorite_product_api/users/services.py
@@ -54,11 +54,11 @@ class UserService:
         return updated_data
 
     @classmethod
-    async def delete_user(cls, db_session: AsyncSession, uuid: UUID) -> bool:
+    async def delete_user(cls, db_session: AsyncSession, uuid: UUID) -> None:
         user = await cls.get_user(db_session=db_session, uuid=uuid)
 
         db_session.delete(user)
 
         await db_session.flush()
 
-        return True
+        return
