b5d54e80b181fdf3ad702affed9617641964f23c
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-29 14:54:05 -0300

    fix: adiciona type hint no método create_user

--- favorite_product_api/users/services.py
+++ favorite_product_api/users/services.py
@@ -5,7 +5,7 @@ from favorite_product_api.users.models import User
 
 class UserService:
     @classmethod
-    async def create_user(cls, db_session: AsyncSession, name, email) -> User:
+    async def create_user(cls, db_session: AsyncSession, name: str, email: str) -> User:
         user = User(email=email, name=name)
         db_session.add(user)
         await db_session.flush()
