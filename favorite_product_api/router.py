08bcb621581551b1835973340c09c37be9e856a6
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-28 22:05:38 -0300

    feat: adiciona rotas de users na v1

--- favorite_product_api/router.py
+++ favorite_product_api/router.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
+from favorite_product_api.users.routes import router as users_router
+
 api_v1_router = APIRouter()
+
+api_v1_router.include_router(users_router, prefix="/users", tags=["Users"])
