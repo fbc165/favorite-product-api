05486bb8f1a17959e9c9444f84bd56d1279824f1
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-29 16:43:47 -0300

    feat: adiciona rota para atualizar um usuário

--- favorite_product_api/users/routes.py
+++ favorite_product_api/users/routes.py
@@ -1,10 +1,13 @@
+from uuid import UUID
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from favorite_product_api.databases.postgresql import get_db
-from favorite_product_api.users.payloads import CreateUserPayload
-from favorite_product_api.users.responses import CreateUserResponse
+from favorite_product_api.exceptions import NotFoundError
+from favorite_product_api.users.payloads import CreateUserPayload, UpdateUserPayload
+from favorite_product_api.users.responses import CreateUserResponse, UpdateUserResponse
 from favorite_product_api.users.services import UserService
 
 router = APIRouter()
@@ -28,6 +31,36 @@ async def create_user(
             status_code=status.HTTP_409_CONFLICT,
             detail="Email already exists",
         )
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        )
+
+
+@router.patch(
+    "/{uuid}", response_model=UpdateUserResponse, response_model_exclude_none=True
+)
+async def update_user(
+    uuid: UUID, payload: UpdateUserPayload, db_session: AsyncSession = Depends(get_db)
+):
+    try:
+        updated_data = await UserService.update_user(
+            db_session=db_session, uuid=uuid, name=payload.name, email=payload.email
+        )
+
+        return UpdateUserResponse(
+            uuid=uuid,
+            email=updated_data.get("email", None),
+            name=updated_data.get("name", None),
+        )
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, detail=str(e)
+        )
+    except NotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
     except Exception:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
