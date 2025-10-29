82fc605b2f1b9f4fc1b4d9b630c2eddad03ad83c
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-29 16:37:34 -0300

    refactor: validador do name

--- favorite_product_api/users/payloads.py
+++ favorite_product_api/users/payloads.py
@@ -1,19 +1,30 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from typing import Annotated
+
+from pydantic import (
+    AfterValidator,
+    BaseModel,
+    EmailStr,
+    Field,
+)
+
+
+def validate_name(name: str) -> str:
+    if not name:
+        raise ValueError("Name cannot be empty")
+
+    prepositions = ["e", "de", "da", "do", "dos", "das"]
+
+    return " ".join(
+        word.capitalize() if word.lower() not in [prepositions] else word.lower()
+        for word in name.strip().split()
+    )
+
+
+NameStr = Annotated[
+    str, Field(max_length=100, min_length=1), AfterValidator(validate_name)
+]
 
 
 class CreateUserPayload(BaseModel):
-    name: str
+    name: NameStr
     email: EmailStr
-
-    @field_validator("name")
-    @classmethod
-    def validate_name(cls, value: str) -> str:
-        if not value:
-            ValueError("name cannot be empty")
-
-        prepositions = ["e", "de", "da", "do", "dos", "das"]
-
-        return " ".join(
-            word.capitalize() if word.lower() not in [prepositions] else word.lower()
-            for word in value.strip().split()
-        )
