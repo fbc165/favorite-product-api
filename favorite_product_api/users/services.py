8d0ae3a6239e8b400cfea4c167106a12953380c9
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-29 16:43:32 -0300

    feat: adiciona service para atualizar um usuário no db

--- favorite_product_api/users/services.py
+++ favorite_product_api/users/services.py
@@ -1,5 +1,9 @@
+from uuid import UUID
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from favorite_product_api.exceptions import NotFoundError
 from favorite_product_api.users.models import User
 
 
@@ -11,3 +15,34 @@ class UserService:
         await db_session.flush()
 
         return user
+
+    @classmethod
+    async def update_user(
+        cls,
+        db_session: AsyncSession,
+        uuid: UUID,
+        name: str | None = None,
+        email: str | None = None,
+    ) -> dict:
+        updated_data = {}
+
+        query = select(User).where(User.uuid == uuid)
+        user = await db_session.execute(query)
+        user = user.scalar_one_or_none()
+
+        if user is None:
+            raise NotFoundError("User not found")
+
+        if name is not None:
+            user.name = name
+            updated_data["name"] = user.name
+
+        if email is not None:
+            user.email = email
+            updated_data["email"] = user.email
+
+        db_session.add(user)
+
+        await db_session.flush()
+
+        return updated_data
