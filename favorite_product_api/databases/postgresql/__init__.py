15d7d21e319844d84a72a248c298a3c59f53b9d3
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-30 23:47:52 -0300

    feat: adiciona model dque representa os produtos favoritos de um usuário

--- favorite_product_api/databases/postgresql/__init__.py
+++ favorite_product_api/databases/postgresql/__init__.py
@@ -1,12 +1,8 @@
 # Os models da API devem ser todos importados aqui
+from favorite_product_api.user_favorite_products.models import UserFavoriteProduct
 from favorite_product_api.users.models import User
 
 from .base import Base
 from .session import engine, get_db
 
-__all__ = [
-    "Base",
-    "engine",
-    "get_db",
-    "User",
-]
+__all__ = ["Base", "engine", "get_db", "User", "UserFavoriteProduct"]
---
+++ favorite_product_api/user_favorite_products/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy.orm import relationship
+
+from favorite_product_api.databases.postgresql.base import Base
+
+
+class UserFavoriteProduct(Base):
+    __tablename__ = "user_favorite_product"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(
+        Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+    product_id = Column(String(50), nullable=False, index=True, unique=True)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+
+    user = relationship(
+        "User",
+        back_populates="favorite_products",
+    )
