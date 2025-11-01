1460a4027bf316864f565b4eb5cafc4acfb4d322
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-31 22:18:39 -0300

    feat: adiciona response model da resposta da api de products

---
+++ favorite_product_api/products/responses.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+from pydantic import BaseModel, HttpUrl
+
+
+class ProductResponse(BaseModel):
+    id: int
+    title: str
+    image: HttpUrl
+    price: float
+    description: str
+    category: str
+    rating: dict[str, Union[float, int]]
