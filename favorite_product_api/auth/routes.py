40aadf8b51adc36d7a1f334d150c15bcfe16f3b8
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-11-01 00:07:26 -0300

    feat: retorna uuid ao autenticar

--- favorite_product_api/auth/routes.py
+++ favorite_product_api/auth/routes.py
@@ -26,4 +26,4 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = create_access_token(data={"sub": user.email})
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "uuid": user.uuid}
