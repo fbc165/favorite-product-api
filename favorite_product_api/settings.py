da88a48a902610d326457093f9d0b226f2122d2d
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-31 21:55:49 -0300

    feat: adiciona variáveis de ambiente

--- favorite_product_api/settings.py
+++ favorite_product_api/settings.py
@@ -4,11 +4,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_HOST: str
     POSTGRES_PORT: str
+    PRODUCT_API_URL: str
+    SECRET_KEY: str
 
     @property
     def DATABASE_URL(self) -> str:
