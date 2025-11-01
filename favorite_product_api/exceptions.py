eadbfdbbfab60e8d50b2ee138bf0e50153c345dc
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-10-31 21:54:55 -0300

    feat: adiciona exceptions

--- favorite_product_api/exceptions.py
+++ favorite_product_api/exceptions.py
@@ -4,3 +4,19 @@ class NotFoundError(Exception):
 
     def __str__(self):
         return self.detail
+
+
+class EmailAlreadyExistsError(Exception):
+    def __init__(self, detail: str = "Email address not available"):
+        self.detail = detail
+
+    def __str__(self):
+        return self.detail
+
+
+class ProductAlreadyIsFavoriteError(Exception):
+    def __init__(self, detail: str = "Product already in user favorite product list"):
+        self.detail = detail
+
+    def __str__(self):
+        return self.detail
