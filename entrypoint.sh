bb6f84bb34b6fecf68e6fe7762b72c56ea136510
Fabricio Alves <fabricio.alves@telavita.com.br>
2025-11-01 00:10:00 -0300

    feat: roda migrations ao subir o app

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "==== Applying database migrations ===="
+alembic -c favorite_product_api/databases/postgresql/alembic.ini upgrade head
 
 echo "\n==== Running application ===="
 uvicorn favorite_product_api.app:app --host 0.0.0.0 --port 9900
